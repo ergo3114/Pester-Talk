0b56b3bdcd3cee5145ddf8b9f44c116e41f130cb
C. David Littlejohn <cdlittlejohn1@gmail.com>
2022-04-05 16:19:53 -0400

    Updated intro

--- 1-Intro.ps1
+++ 1-Intro.ps1
@@ -3,3 +3,9 @@ Install-Module Pester
 Install-Module Pester -Force
 
 Update-Module Pester
+
+cd C:\git\Pester-Talk
+
+# Unit Testing = Testing of individual functions
+# Infrastructure Testing = Testing of the infrastructure
+# User Testing = Testing the user experience of your script/function
