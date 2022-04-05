c73bb17d271976ed549907ec037767a05e71b2f0
C. David Littlejohn <cdlittlejohn1@gmail.com>
2022-04-05 16:17:33 -0400

    Added more entry examples

--- 2-MyFirstTest.ps1
+++ 2-MyFirstTest.ps1
@@ -3,6 +3,15 @@ Describe "My first pester" {
         It "Should return true" {
             $true | Should -Be $true
         }
+
+        It "The words should be the same" {
+            "This is a test" | Should -BeExactly "This is a test"
+        }
+        
+        It "Number comparison" {
+            5 | Should -BeGreaterThan 4
+            5 | Should -BeGreaterOrEqual 5
+        }
     }
     #Context "Learn about context error" {
     #    It "Should return true" {
